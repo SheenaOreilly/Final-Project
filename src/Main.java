2d016f7b84d7d54a813ddff1dc7701fe06ba9497
Sheena OReilly <oreils21@tcd.ie>
2022-03-30 17:04:45 +0100

    fixing white space error

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.TST;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -40,6 +38,7 @@ public class Main
 
             scanner.useDelimiter(",\\s*");
             temp = scanner.next();
+            temp = temp.trim();
             String newString = "";
             if(res[res.length - 1].equals("WB") || res[res.length - 1].equals("EB") || res[res.length - 1].equals("SB") || res[res.length - 1].equals("NB") || res[res.length - 1].equals("FLAGSTOP"))
             {
@@ -63,6 +62,13 @@ public class Main
             myTST.put(result, i);
 
         }
+
+        // print results
+            StdOut.println("keys(\"\"):");
+            for (String key : myTST.keys()) {
+                StdOut.println(key + " " + myTST.get(key));
+            }
+            StdOut.println();
 
             StdOut.println("The stop will be formatted as:");
             StdOut.println("stop name, stop id, stop code, stop desc, stop lat, stop_lon, zone id, stop url, location type, parent station");
